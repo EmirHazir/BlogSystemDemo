543cf1686a9c8002b019fd044adf1bda1f20d137
EmirHazir <emircanaziz@gmail.com>
2017-01-26 00:20:36 +0300

    Demo private mvc demo

---
+++ BlogSystemDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlogSystemDemo.Startup))]
+namespace BlogSystemDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
